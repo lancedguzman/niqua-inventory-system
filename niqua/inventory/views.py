779cb23529b8743b6263e228a73cb9fd64220529
Lance B. De Guzman <137474979+lancedguzman@users.noreply.github.com>
2025-04-20 00:48:39 +0800

    3 - Materials List
    It can now show all materials. You can add, edit, and delete materials. It can accept both textile and accessory.

--- niqua/inventory/views.py
+++ niqua/inventory/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Product
-from .forms import ProductForm, EditForm
+from .models import Product, Accessory, Textile
+from .forms import *
 
 @login_required
 def dashboard(request):
@@ -17,7 +17,7 @@ def product_list(request):
 
 
 def product_form(request):
-    """Handles product creation."""
+    """Displays page to create product."""
     if request.method == "POST":
         form = ProductForm(request.POST)
         if form.is_valid():
@@ -35,14 +35,14 @@ def product_edit(request, product_id):
     product = Product.objects.get(id=product_id)
 
     if (request.method == "POST"):
-        form = EditForm(request.POST, instance=product)
+        form = EditProductForm(request.POST, instance=product)
 
         if form.is_valid():
             form.save()
             return redirect("product-list")
     
     else:
-        form = EditForm(instance=product)
+        form = EditProductForm(instance=product)
 
     return render(request, "product_edit.html",
                   {"edit_form": form})
@@ -60,9 +60,80 @@ def job_orders(request):
     return render(request, "job_orders.html")
 
 
-def materials_list(request):
+def material_list(request):
     """Displays the list of available materials."""
-    return render(request, "materials_list.html")
+    textiles = Textile.objects.all()
+    accessories = Accessory.objects.all()
+    return render(request, "material_list.html", {
+        "textiles": textiles,
+        "accessories": accessories,
+    })
+
+
+def material_form(request, material_type):
+    """Displays form to add material."""
+    model_map = {
+        "textile": (Textile, TextileForm),
+        "accessory": (Accessory, AccessoryForm),
+    }
+
+    model_form_pair = model_map.get(material_type.lower())
+    model, form_class = model_form_pair
+
+    if (request.method == "POST"):
+        form = form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("material-list")
+        
+    else:
+        form = form_class()
+
+    return render(request, "material_form.html", {
+        "create_form": form,
+        "material_type": material_type,
+    })
+
+
+def material_edit(request, material_type, pk):
+    """Displays form to edit material."""
+    model_map = {
+        "textile": (Textile, EditTextileForm),
+        "accessory": (Accessory, EditAccessoryForm),
+    }
+
+    model_form_pair = model_map.get(material_type.lower())
+    model, form_class = model_form_pair
+    instance = model.objects.filter(pk=pk).first()
+
+    if (request.method == "POST"):
+        form = form_class(request.POST, instance=instance)
+
+        if form.is_valid():
+            form.save()
+            return redirect("material-list")
+        
+    else:
+        form = form_class(instance=instance)
+
+    return render(request, "material_edit.html", {
+        "edit_form": form,
+        "material_type": material_type,
+    })
+
+
+def material_delete(request, material_type, pk):
+    """Delete a material from material list."""
+    model_map = {
+        "textile": Textile,
+        "accessory": Accessory,
+    }
+
+    model = model_map.get(material_type.lower())
+    material = get_object_or_404(model, pk=pk)
+    material.delete()
+
+    return redirect("material-list")
 
 
 def stock_list(request):
